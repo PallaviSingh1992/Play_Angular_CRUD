c820fe2e3ed6dea6bf6447b0401fea971ddc5e1a
PallaviSingh1992 <pallavisingh_1992@yahoo.co.in>
2016-03-14 18:17:23 +0530

    Updated update

--- public/javascripts/add.js
+++ public/javascripts/add.js
@@ -1,17 +1,7 @@
 
-var internApp = angular.module('internApp', ['ngRoute']);
-        internApp.controller('Add', function ($scope,$http){
-        alert("hello");
-        $scope.saveIt = function(){
-        alert("hi");
-        var data = {"id":parseInt($scope.id),"name":$scope.name,"email":$scope.email,"mobile":$scope.mobile,"address":$scope.address,"emergency":$scope.emergency};
-          $http({
-                          method: 'POST',
-                          url:'/insert',
-                          data: JSON.stringify(data),
-                          contentType: "application/json",
-                          dataType: "json"
-                      });
-        }
+var internApp = angular.module('internApp', []);
+internApp.controller('Add', function ($scope, $http){
+$scope.addTo = function(){
 
-      });
+alert("hello")
+}})
