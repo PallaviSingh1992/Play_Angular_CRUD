577c1c0f19438a43321c37589c195794ec1f6f6d
akashsethi24 <akashsethi@hotmail.com>
2016-03-14 16:47:59 +0530

    Add js

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,3 +1,17 @@
 if (window.console) {
   console.log("Welcome to your Play application's JavaScript!");
 }
+
+     var internApp = angular.module('internApp', []);
+        internApp.controller('InternCtrl', function ($scope,$http){
+        $scope.saveIt = function(){
+        var data = {"id":parseInt($scope.id),"name":$scope.name,"email":$scope.email,"address":$scope.address,"mobile":$scope.mobile,"emergency":$scope.emergency};
+          $http({
+                          method: 'POST',
+                          url:'http://localhost:9000/student/save',
+                          data: JSON.stringify(data),
+                          contentType: "application/json",
+                          dataType: "json"
+                      });
+        }
+      });
