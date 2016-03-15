f31cb7c844d3ae2935303235be57690cb42b3048
PallaviSingh1992 <pallavisingh_1992@yahoo.co.in>
2016-03-15 17:54:06 +0530

    Final Version

--- public/javascripts/update.js
+++ public/javascripts/update.js
@@ -1,12 +1,12 @@
 
 var internApp = angular.module('internApp', []);
 
-    alert("Coming here 1");
+
         internApp.controller('UpdateController', function ($scope, $http){
         $scope.updateIt = function(){
 
        var data1 = {"id":parseInt($scope.id),"name":$scope.name,"email":$scope.email,"mobile":$scope.mobile,"address":$scope.address,"emergency":$scope.emergency};
-       alert(data1);
+
                       window.location = '/showlist';
        return $http({
                      method:'POST',
@@ -17,13 +17,14 @@ var internApp = angular.module('internApp', []);
                  })
 
         }
+
         })
 
         internApp.controller('DeleteController', function ($scope, $http){
                   $scope.Delete=function(){
-                    alert("eew");
+
                     $http.get('/delete/'+$scope.id).success(function(data) {
-                    alert("deleted" )});
+                    alert("Deleted "+$scope.id+" successfully" )});
                     window.location = '/showlist'
                 }
         })
